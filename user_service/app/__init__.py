5be6714803f7066a9acc516221b744cf46729928
Ermadragon <sas02nrvs@gmail.com>
2025-05-01 12:26:25 +0530

    Create __init__.py

---
+++ user_service/app/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+from .config import Config
+
+db = SQLAlchemy()
+jwt = JWTManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    jwt.init_app(app)
+
+    from .routes import user_bp
+    app.register_blueprint(user_bp)
+
+    return app
