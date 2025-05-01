e6e91076fa45e510619ed1bbbaf221bfb1187111
Ermadragon <sas02nrvs@gmail.com>
2025-05-01 12:28:27 +0530

    Create models.py

---
+++ user_service/app/models.py
@@ -0,0 +1,8 @@
+from . import db
+import uuid
+
+class User(db.Model):
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    preferences = db.Column(db.JSON, nullable=True)
