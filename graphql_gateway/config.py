1af4422ce23ff2828a768f65b38dabe9a169cd1c
Ermadragon <sas02nrvs@gmail.com>
2025-05-01 12:50:10 +0530

    Create config.py

---
+++ graphql_gateway/config.py
@@ -0,0 +1,4 @@
+USER_SERVICE_URL = 'http://user_service:5001'
+NOTIF_SERVICE_URL = 'http://notification_service:5002'
+RECOMM_SERVICE_URL = 'http://recommendation_service:5003'
+JWT_SECRET = 'supersecretkey'
