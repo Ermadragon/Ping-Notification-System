865003d0efe2a71ba12bcebbd9055c59531db2d1
Ermadragon <sas02nrvs@gmail.com>
2025-05-01 12:54:39 +0530

    Update __init__.py

--- graphql_gateway/__init__.py
+++ graphql_gateway/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from .schema import schema
 from strawberry.flask.views import GraphQLView
 from .auth import jwt_required_graphql
+from .context import get_context
 
 def create_app():
     app = Flask(__name__)
@@ -12,6 +13,7 @@ def create_app():
             "graphql_view",
             schema=schema,
             graphiql=True,
+            context_getter=get_context,
             decorators=[jwt_required_graphql],
         ),
     )
